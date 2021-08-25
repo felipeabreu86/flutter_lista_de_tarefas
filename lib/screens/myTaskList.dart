57df3c1198260e076b32add3eda6ab3ce6205ecd
Felipe Abreu <felipeabreu.rj@gmail.com>
2021-08-25 14:08:25 -0300

    Se aplicado, este commit irá criar a estrutura inical de telas

---
+++ lib/screens/myTaskList.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyTaskList extends StatefulWidget {
+  const MyTaskList({Key? key}) : super(key: key);
+
+  @override
+  _MyTaskListState createState() => _MyTaskListState();
+}
+
+class _MyTaskListState extends State<MyTaskList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
