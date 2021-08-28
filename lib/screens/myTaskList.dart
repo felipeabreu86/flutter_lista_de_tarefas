5ec47171879591406d4f6c2b2cf354e99852ad70
Felipe Abreu <felipeabreu.rj@gmail.com>
2021-08-28 08:41:29 -0300

    Se aplicado, este commit irá iniciar o desenvolvimento da tela de tarefas

--- lib/screens/myTaskList.dart
+++ lib/screens/myTaskList.dart
@@ -10,6 +10,32 @@ class MyTaskList extends StatefulWidget {
 class _MyTaskListState extends State<MyTaskList> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lista de Tarefas"),
+      ),
+      body: Text("Conteúdo"),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton.extended(
+        backgroundColor: Colors.purple,
+        foregroundColor: Colors.white,
+        elevation: 6,
+        icon: Icon(Icons.add_shopping_cart),
+        label: Text("Adicionar"),
+        onPressed: () {
+          print("Botão pressionado!");
+        },
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.add),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
